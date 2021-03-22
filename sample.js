be34188a61e4959732f1f2261f96b6eacb692ba5
niteesh99 <70364513+niteesh99@users.noreply.github.com>
2021-03-22 16:37:14 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,34 @@
+class User {
+    constructor(email, name){
+        this.email = email;
+        this.name = name;
+        this.score = 0;
+    }
+    login(){
+        console.log(this.email, 'just logged in');
+    }
+    logout(){
+        console.log(this.email, 'just logged out');
+            }
+    updateScore(){
+        this.score++;
+        console.log(this.email, 'score is now', this.score);
+        
+    }
+}
+
+class Admin extends User {
+    deleteUser(user){
+        users = users.filter(u => {
+            return u.email != user.email
+        });
+    }
+}
+
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@mariokorp.com', 'Yoshi');
+var admin = new Admin('shaun@ninjas.com', 'Shaun');
+
+var users = [userOne, userTwo, admin];
+deleteUser(userOne);
+
